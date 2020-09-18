5ab7a22df65d8c2882f75562c7dea0374273601f
Michael Korobkov <korobkov200364@gmail.com>
2020-09-18 19:31:22 +0300

    Update 4.4.cpp

--- 1 Семестр/ДЗ/4.4.cpp
+++ 1 Семестр/ДЗ/4.4.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-  for (int i = 0; i < 13; ++i)
-  {
-      if (i >= 1 && i <= 6)
-      {
-          for (int j = 0; j < 8; ++j)
-              cout << "*";
-          for (int j = 8; j < 49; ++j)
-              cout << "_";
-      }
-      else
-          for (int j = 0; j < 49; ++j)
-              cout << "_";
-      cout << endl;
-  }
-  return 0;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    for (int i = 0; i < 13; ++i)
+    {
+        WORD stripeColor = (i % 2) ? (240) : (79);
+        if (i >= 0 && i <= 5)
+        {
+            SetConsoleTextAttribute(hConsole, 31);
+            for (int j = 0; j < 8; ++j)
+                cout << "* ";
+            SetConsoleTextAttribute(hConsole, stripeColor);
+            for (int j = 8; j < 49-8; ++j)
+                cout << " ";
+        }
+        else
+        {
+            SetConsoleTextAttribute(hConsole, stripeColor);
+            for (int j = 0; j < 49; ++j)
+                cout << " ";
+        }
+        cout << endl;
+    }
+    SetConsoleTextAttribute(hConsole, 15);
+    return 0;
 }
