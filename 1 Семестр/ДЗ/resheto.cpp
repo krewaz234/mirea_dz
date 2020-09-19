42c3b5d7e80410efd8b772ac9a17a78aee4245a3
Michael Korobkov <korobkov200364@gmail.com>
2020-09-19 12:07:53 +0300

    Create resheto.cpp

---
+++ 1 Семестр/ДЗ/resheto.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+void resheto(size_t n)
+{
+	++n;
+	bool* arr = new bool[n];
+	for (size_t i = 0; i < n; ++i)
+		arr[i] = true;
+	for (size_t i = 2; i < n; ++i)
+	{
+		if (arr[i])
+		{
+			for (size_t j = i*2; j < n; j += i)
+				arr[j] = false;
+		}
+	}
+	for (size_t i = 2; i < n; ++i)
+		if (arr[i])
+			cout << i << " ";
+}
+
+int main()
+{
+	resheto(100);
+	return 0;
+}
