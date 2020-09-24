98984aa2381eb8780bb0507f731788151f396f9e
Michael Korobkov <korobkov200364@gmail.com>
2020-09-24 12:17:51 +0300

    Update 4.1.cpp

--- 1 Семестр/ДЗ/4.1.cpp
+++ 1 Семестр/ДЗ/4.1.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <ctime>
+
+#define NUMS 3
 
 using namespace std;
 
 int main()
 {
+    srand(time(0));
     setlocale(LC_ALL, "Russian");
     ofstream fout("file.txt");
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < NUMS; ++i)
     {
-        int n = rand() % 100;
+        int n = rand() % 10;
         fout << n << endl;
         cout << "В файл записано число " << n << endl;
     }
     fout.close();
     ifstream fin("file.txt");
     int sum = 0;
-    while (!fin.eof())
+    while (true)
     {
         int n;
         fin >> n;
+        if (fin.eof()) break;
         sum += n;
     }
+    fin.close();
     cout << "Сумма чисел в файле: " << sum << endl;
     return 0;
 }
