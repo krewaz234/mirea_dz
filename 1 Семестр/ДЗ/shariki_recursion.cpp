c294fbfa5abdc0540f396260c7fa2e6d62bcbce8
Michael Korobkov <korobkov200364@gmail.com>
2020-10-07 20:05:00 +0300

    Create shariki_recursion.cpp

---
+++ 1 Семестр/ДЗ/shariki_recursion.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+size_t counter = 0;
+void rec(int *balls, size_t k = 0)
+{
+    if (k == 10)
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            if (balls[i] == i)
+            {
+                ++counter;
+                return;
+            }
+        }
+        return;
+    }
+    for (int i = 0; i < 10; ++i)
+    {
+        bool f = false;
+        for (int j = 0; j < k; ++j)
+        {
+            if (i == balls[j])
+            {
+                f = true;
+                break;
+            }
+        }
+        if (!f)
+        {
+            balls[k] = i;
+            rec(balls, k + 1);
+            balls[k] = -1;
+        }
+    }
+}
+
+int main()
+{
+    int *balls = new int[10];
+    for (int i = 0; i < 10; ++i)
+        balls[i] = -1;
+    rec(balls);
+    cout << counter;
+    return 0;
+}
