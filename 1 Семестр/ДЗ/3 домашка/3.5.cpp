09a8be65774e8af18db9960c49ea929bf4b69b98
Michael Korobkov <korobkov200364@gmail.com>
2020-10-07 23:53:25 +0300

    Update 3.5.cpp

--- 1 Семестр/ДЗ/3 домашка/3.5.cpp
+++ 1 Семестр/ДЗ/3 домашка/3.5.cpp
@@ -1,20 +1,45 @@
 #include <iostream>
 #include <ctime>
 #include <algorithm>
-
-#define SZ 30
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
+    //устанавливаем полную поддержку русского языка
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
+    //обновляем ядро псевдорандома
     srand(time(0));
+
     string s;
-    for (int i = 0; i < SZ; ++i)
-        s += rand() % 26 + 65;
-    cout << "Исходная строка: " << s << endl;
-    sort(s.begin(), s.end());
-    cout << "Отсортированная строка: " << s << endl;
+    cout << "Введите строку:" << endl;
+    getline(cin, s);
+    //сортировка
+    for (size_t i = 0; i < s.length() - 1; ++i)
+    {
+        for (size_t j = i + 1; j < s.length(); ++j)
+        {
+            if (s[i] > s[j])
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+
+    //удаление повторяющихся символов
+    for (size_t i = 0; i < s.length() - 1; ++i)
+        if (s[i] == s[i + 1])
+            s.erase(i--, 1);
+
+    //удаление небуквенных символов
+    for (size_t i = 0; i < s.length(); ++i)
+        if (!isalpha(s[i]))
+            s.erase(i--, 1);
+    cout << "Преобразованная строка: " << s << endl;
     return 0;
 }
