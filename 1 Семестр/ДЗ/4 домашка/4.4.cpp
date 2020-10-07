4a14f917f6d6ea177dbf21bb8f673cedb1bedd9e
Michael Korobkov <korobkov200364@gmail.com>
2020-10-07 22:35:29 +0300

    Update 4.4.cpp

--- 1 Семестр/ДЗ/4 домашка/4.4.cpp
+++ 1 Семестр/ДЗ/4 домашка/4.4.cpp
@@ -10,12 +10,17 @@ using namespace std;
 
 int main()
 {
+    //хэндл консоли чтобы можно было изменять параметры выводимого текста
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     for (int i = 0; i < 13; ++i)
     {
+        //тип данных WORD это то же самое что и unsigned short
+        //если не понимаете что за вопросительные знаки с двоеточиями почитайте:
+        //https://ravesli.com/urok-41-sizeof-zapyataya-i-uslovnyj-ternarnyj-operator/#toc-2
         WORD stripeColor = (i % 2) ? (RED) : (WHITE);
         if (i >= 0 && i <= 5)
         {
+            //устанавливаем цветовые параметры для вывода текста
             SetConsoleTextAttribute(hConsole, BLUE_BG);
             for (int j = 0; j < 8; ++j)
                 cout << "* ";
@@ -31,6 +36,7 @@ int main()
         }
         cout << endl;
     }
+    //возвращаем дефолтные значения (не обязательно)
     SetConsoleTextAttribute(hConsole, STANDART_COLOR_SCHEME);
     return 0;
 }
