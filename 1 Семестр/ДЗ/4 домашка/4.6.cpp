e0d903ed6b803d59abe68ceb83f1ecd6e2fc5a44
Michael Korobkov <korobkov200364@gmail.com>
2020-10-10 10:32:48 +0300

    Update 4.6.cpp

--- 1 Семестр/ДЗ/4 домашка/4.6.cpp
+++ 1 Семестр/ДЗ/4 домашка/4.6.cpp
@@ -24,14 +24,69 @@ int char_to_number(char c)
     }
 }
 
+int digit(char c)
+{
+    if (c == 'I' || c == 'V' || c == 'X')
+        return 0;
+    if (c == 'L' || c == 'C' || c == 'D')
+        return 1;
+    return 2;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
     cout << "Введите число в римской системе счисления: ";
     string s;
-    cin >> s;
+    getline(cin, s);
     for (int i = 0; i < s.length(); ++i)
         s[i] = toupper(s[i]); //все символы делаем большими
+    //проверяем что все символы корректны
+    for (int i = 0; i < s.length(); ++i)
+    {
+        string l = "IVXLCDM";
+        if (l.find(s[i]) == string::npos)
+        {
+            cerr << "Введен неверный символ. Перезапустите программу и повторите ввод.";
+            return 0;
+        }
+    }
+    //проверяем на 3 одинаковых, стоящих в 1 разряде
+    if (s.length() > 2)
+    {
+        for (int i = 0; i < s.length() - 3; ++i)
+        {
+            if (digit(s[i]) != 2
+            &&  digit(s[i]) == digit(s[i + 1])
+            &&  digit(s[i + 1]) == digit(s[i + 2])
+            &&  digit(s[i + 2]) == digit(s[i + 3]))
+            {
+                cerr << "Введено неверное число (по правилу записи римских чисел, рядом не могут стоять 3 цифры одинакового разряда). Перезапустите программу и повторите ввод.";
+                return 0;
+            }
+        }
+    }
+    //проверяем, что числа одного разряда стоят рядом
+    bool is_digit_entered[] = {0, 0, 0};
+    int prev = digit(s[0]);
+    for (int i = 1; i < s.length(); ++i)
+    {
+        if (is_digit_entered[digit(s[i])])
+        {
+            cerr << "Цифры, харакетризующие один разряд должны стоять рядом. Перезапустите программу и повторите ввод.";
+            return 0;
+        }
+        if (digit(s[i]) < prev)
+            is_digit_entered[prev] = 1;
+        else
+            if (digit(s[i]) > prev)
+            {
+                cerr << "Разряды в записи римского числа должны быть расположены по убыванию. Перезапустите программу и повторите попытку ввода.";
+                return 0;
+            }
+        prev = digit(s[i]);
+    }
+    //перевод
     int result = 0; //конечный результат
     for (size_t i = 0; i < s.length() - 1; ++i)
     {
@@ -45,7 +100,7 @@ int main()
         else
             result += n1;
     }
-    result += char_to_number(s.back()); //последняя цифра всегда прибавляется
+    result += char_to_number(s[s.length() - 1]); //последняя цифра всегда прибавляется
     cout << "Его представление в десятичном виде: " << result << endl;
     return 0;
 }
